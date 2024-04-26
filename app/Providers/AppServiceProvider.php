b389af9d362ed0a2ce2c80b91524b51a153b4c64
Anatoly Meshkov <anatoly@sgtautotransport.com>
2024-04-26 15:47:30 +0300

    room_status validatod added globally

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Http\Enums\RoomStatus;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,5 +23,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         JsonResource::withoutWrapping();
+
+        Validator::extend('room_status', function ($attribute, $value) {
+            return RoomStatus::isValid($value);
+        }, 'Incorrect Room Status value. Only ' . implode(', ', RoomStatus::values()) . ' values allowed');
+
     }
 }
