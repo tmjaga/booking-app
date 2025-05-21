61f7c88ccea0f9d3908fac5f23ac1de67f8f6505
Anatoly Meshkov <tmjaga@gmail.com>
2025-05-21 12:11:39 +0300

    Payment tests rewritten

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -9,14 +9,19 @@ class Payment extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['booking_id', 'amount', 'payment_date' , 'status'];
+    protected $fillable = ['booking_id', 'amount', 'payment_date', 'status'];
 
     protected static function booted()
     {
         static::saving(function ($model) {
-            if (!$model->payment_date) {
+            if (! $model->payment_date) {
                 $model->payment_date = now()->format('Y-m-d H:i:s');
             }
         });
+    }
+
+    public function booking()
+    {
+        return $this->belongsTo(Booking::class);
     }
 }
---
+++ tests/Feature/PaymentTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Booking;
+use App\Models\Payment;
+use App\Models\User;
+use Tests\TestCase;
+
+beforeEach(function () {
+    $this->user = User::factory()->create();
+    $this->actingAs($this->user);
+    $this->booking = Booking::factory()->create();
+});
+
+it('returns payments for a booking', function () {
+    //$booking = Booking::factory()->create();
+
+    Payment::factory()
+        ->count(2)
+        ->for($this->booking)
+        ->create();
+
+    $response = $this->getJson("/api/bookings/{$this->booking->id}/payments");
+
+    $response->assertOk()->assertJsonStructure([
+        '*' => ['id', 'booking_id', 'amount', 'status'],
+    ])
+        ->assertJsonCount(2);
+});
+
+it('stores a payment for a booking', function () {
+    $booking = Booking::factory()->create();
+
+    $data = Payment::factory()->make([
+        'booking_id' => $booking->id,
+    ])->toArray();
+
+    $response = $this->postJson("/api/bookings/{$booking->id}/payments", $data);
+
+    $response->assertCreated()->assertJsonStructure([
+        'data' => ['id', 'booking_id', 'amount', 'status'],
+    ]);
+});
